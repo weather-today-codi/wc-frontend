fb53c5eabeab97d80958824f1364e3f62fd04cd1
alpha0212 <qkrcjfgml321@naver.com>
2023-08-04 16:28:54 +0900

    feat(components): add CardButton component

---
+++ src/components/Main/CardButton/styled.ts
@@ -0,0 +1,46 @@
+import styled from "@emotion/styled";
+
+export const CardElement = styled.button`
+  width: 11rem;
+  height: 11rem;
+
+  border: none;
+  border-radius: 0.9rem;
+  background-color: #fafafc;
+  margin: auto;
+
+  cursor: pointer;
+
+  :active {
+    transform: scale(0.98);
+  }
+  transition: transform 0.2s;
+
+  font-family: "Pretendard Variable";
+`;
+
+export const CardInner = styled.div`
+  padding: 1.2rem 0.9rem;
+
+  width: 100%;
+  height: 100%;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+export const CardImage = styled.img`
+  width: 3rem;
+  height: 3rem;
+
+  margin-top: 0.5rem;
+`;
+
+export const CardText = styled.p`
+  font-size: 1.6rem;
+  font-weight: 700;
+  text-align: left;
+  color: #505050;
+
+  margin-top: 3rem;
+`;
