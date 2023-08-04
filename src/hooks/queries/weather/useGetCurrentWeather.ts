338009946faa55421cdff084095e7dca0f2d315e
alpha0212 <qkrcjfgml321@naver.com>
2023-08-04 16:32:55 +0900

    feat(hooks): set getting weather hook

--- src/hooks/queries/weather/useGetCurrentWeather.ts
+++ src/hooks/queries/weather/useGetCurrentWeather.ts
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 
 import { getCurrentWeather } from "../../../apis";
 
-export const useGetCurrentWeather = (city: string, apiKey: string) => {
+export const useGetCurrentWeather = (city: string, apiKey?: string) => {
   return useQuery({
     queryKey: ["weather"],
     queryFn: () => getCurrentWeather(city, apiKey),
