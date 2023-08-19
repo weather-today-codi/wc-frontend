fe1897e5bb045794679a3deecf791956dfb11d6d
alpha0212 <qkrcjfgml321@naver.com>
2023-08-20 01:47:50 +0900

    feat: add Button component

---
+++ src/components/common/Button/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import * as S from "./styled";
+
+export type ButtonSize = "small" | "medium" | "large";
+
+export interface ButtonCustomProps {
+  size?: ButtonSize;
+  children: React.ReactNode;
+}
+
+export type ButtonProps = ButtonCustomProps &
+  React.ButtonHTMLAttributes<HTMLButtonElement>;
+
+const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
+  ({ size = "medium", children }, ref) => {
+    return <S.ButtonElement ref={ref}>{children}</S.ButtonElement>;
+  }
+);
+
+Button.displayName = "Button";
+
+export { Button };
