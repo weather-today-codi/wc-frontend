70cbf49626c1c4be857ce47264f6b91d44799c8a
alpha0212 <qkrcjfgml321@naver.com>
2023-08-20 01:51:43 +0900

    chore: absoulte path setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
 import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      react: path.resolve(__dirname, "./node_modules/react"),
+      "@": path.resolve(__dirname, "src/"),
+    },
+  },
   plugins: [
     react(),
     VitePWA({
